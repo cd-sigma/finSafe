9bcdb456b847d0bc40374e1b7138080f5bf7a4e9
cd-sigma <mohammad.numan@clickdee.com>
2023-12-09 01:15:22 +0530

    addedd get nonce

--- backend/api/controllers/user.controller.js
+++ backend/api/controllers/user.controller.js
@@ -1,9 +1,54 @@
+const responseLib = require("../../lib/response.lib")
+const mongoLib = require("../../lib/mongo.lib")
+
+const validatorUtil = require("../../util/validators.util")
+const positionModel = require("../../model/position.model")
+
+const resStatusEnum = require("../../enum/res.status.enum")
+
 async function getUserPositions(req, res) {
     try {
-        const {userAddress} = req.params
-        const user = await userService.getUserPositions(userAddress)
-        return responseLib.sendResponse(res, user, null, 200)
+        let {userAddress} = req.params
+
+        if (validatorUtil.isEmpty(userAddress)) {
+            return responseLib.sendResponse(res, null, "Missing user address", resStatusEnum.VALIDATION_ERROR)
+        }
+
+        userAddress = userAddress.toLowerCase()
+        const userPosition = await mongoLib.findByQuery(positionModel, {userAddress: userAddress})
+        if (validatorUtil.isEmpty(userPosition)) {
+            return responseLib.sendResponse(res, null, "No positions found", resStatusEnum.NOT_FOUND)
+        }
+
+        return responseLib.sendResponse(res, userPosition, null, resStatusEnum.SUCCESS)
+
     } catch (error) {
         return responseLib.sendResponse(res, null, error, 500)
     }
+}
+
+async function getNonce(req, res) {
+    try {
+        let {userAddress} = req.params
+
+        if (validatorUtil.isEmpty(userAddress)) {
+            return responseLib.sendResponse(res, null, "Missing user address", resStatusEnum.VALIDATION_ERROR)
+        }
+
+        userAddress = userAddress.toLowerCase()
+
+        const userPosition = await mongoLib.findByQuery(positionModel, {userAddress: userAddress})
+        if (validatorUtil.isEmpty(userPosition)) {
+            return responseLib.sendResponse(res, null, "No positions found", resStatusEnum.NOT_FOUND)
+        }
+
+        return responseLib.sendResponse(res, userPosition, null, resStatusEnum.SUCCESS)
+
+    } catch (error) {
+        return responseLib.sendResponse(res, null, error, 500)
+    }
+}
+
+module.exports = {
+    getUserPositions: getUserPositions, getNonce: getNonce
 }
