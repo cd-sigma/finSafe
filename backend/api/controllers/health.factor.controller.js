3b8b451a1fe9b2645bd33d3703e8106d2fda39d3
cd-sigma <mohammad.numan@clickdee.com>
2023-12-10 01:30:47 +0530

    liqudatio threshold

--- backend/api/controllers/health.factor.controller.js
+++ backend/api/controllers/health.factor.controller.js
@@ -6,11 +6,89 @@ const validatorUtil = require("../../util/validators.util")
 const positionModel = require("../../model/position.model")
 
 const resStatusEnum = require("../../enum/res.status.enum")
+const web3Lib = require("../../lib/web3.lib");
 
 
 async function calculateHealthFactor(req, res) {
     try {
+        let web3 = await web3Lib.getWebSocketWeb3Instance(process.env.ETH_NODE_WS_URL);
         let {address} = req.params
+        let {
+            usdtAmount,
+            usdtPrice,
+            wbtcAmount,
+            wbtcPrice,
+            wethAmount,
+            wethPrice,
+            yfiAmount,
+            yfiPrice,
+            zrxAmount,
+            zrxPrice,
+            uniAmount,
+            uniPrice,
+            aaveAmount,
+            aavePrice,
+            batAmount,
+            batPrice,
+            busdAmount,
+            busdPrice,
+            daiAmount,
+            daiPrice,
+            enjAmount,
+            enjPrice,
+            kncAmount,
+            kncPrice,
+            linkAmount,
+            linkPrice,
+            manaAmount,
+            manaPrice,
+            mkrAmount,
+            mkrPrice,
+            renAmount,
+            renPrice,
+            snxAmount,
+            snxPrice,
+            susdAmount,
+            susdPrice,
+            tusdAmount,
+            tusdPrice,
+            usdcAmount,
+            usdcPrice,
+            crvAmount,
+            crvPrice,
+            balAmount,
+            balPrice,
+            xsushiAmount,
+            xsushiPrice,
+            renfilAmount,
+            renfilPrice,
+            raiAmount,
+            raiPrice,
+            amplAmount,
+            amplPrice,
+            usdpAmount,
+            usdpPrice,
+            dpiAmount,
+            dpiPrice,
+            fraxAmount,
+            fraxPrice,
+            feiAmount,
+            feiPrice,
+            stethAmount,
+            stethPrice,
+            ensAmount,
+            ensPrice,
+            gusdAmount,
+            gusdPrice,
+            ustAmount,
+            ustPrice,
+            cvxAmount,
+            cvxPrice,
+            oneinchAmount,
+            oneinchPrice,
+            lusdAmount,
+            lusdPrice
+        } = req.body
 
         if (validatorUtil.isEmpty(address)) {
             return responseLib.sendResponse(res, null, "Missing user address", resStatusEnum.VALIDATION_ERROR)
@@ -24,7 +102,16 @@ async function calculateHealthFactor(req, res) {
             return responseLib.sendResponse(res, null, "No position found", resStatusEnum.VALIDATION_ERROR)
         }
 
-        const healthFactor = await healthFactorResolver.calculateHealthFactor(position)
+        const healthFactor = await healthFactorResolver.calculateHealthFactor(position, web3, usdtAmount,
+            usdtPrice, wbtcAmount, wbtcPrice, wethAmount, wethPrice, yfiAmount, yfiPrice, zrxAmount, zrxPrice, uniAmount,
+            uniPrice, aaveAmount, aavePrice, batAmount, batPrice, busdAmount, busdPrice, daiAmount, daiPrice,
+            enjAmount, enjPrice, kncAmount, kncPrice, linkAmount, linkPrice, manaAmount, manaPrice, mkrAmount,
+            mkrPrice, renAmount, renPrice, snxAmount, snxPrice, susdAmount, susdPrice, tusdAmount, tusdPrice,
+            usdcAmount, usdcPrice, crvAmount, crvPrice, balAmount, balPrice, xsushiAmount, xsushiPrice, renfilAmount,
+            renfilPrice, raiAmount, raiPrice, amplAmount, amplPrice, usdpAmount, usdpPrice, dpiAmount, dpiPrice,
+            fraxAmount, fraxPrice, feiAmount, feiPrice, stethAmount, stethPrice, ensAmount, ensPrice, gusdAmount,
+            gusdPrice, ustAmount, ustPrice, cvxAmount, cvxPrice, oneinchAmount, oneinchPrice, lusdAmount, lusdPrice
+        )
 
         return responseLib.sendResponse(res, healthFactor, null, resStatusEnum.SUCCESS)
 
