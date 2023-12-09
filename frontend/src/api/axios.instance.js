e1588ae0a448fd767836ba4e5edff7bfdb4f70f0
kunalmkv <138765481+cd-nikhil@users.noreply.github.com>
2023-12-09 21:59:35 +0530

    frontend|stable|setting up api

--- frontend/src/api/axios.instance.js
+++ frontend/src/api/axios.instance.js
@@ -4,11 +4,11 @@ const token = localStorage.getItem("token") || "";
 axios.defaults.headers.post["Authorization"] = `Bearer ${token}`;
 
 let userAxios = axios.create({
-    baseURL: "http://localhost:3001/user/"
-})
-
-let tokenAxios = axios.create({
-    baseURL: "http://localhost:3001/token/"
+  baseURL: "http://localhost:3001/user/",
 });
 
-export  { userAxios, tokenAxios };
+let tokenAxios = axios.create({
+  baseURL: "http://localhost:3001/token/",
+});
+
+export { userAxios, tokenAxios };
