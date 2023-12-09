49635a6fbfb6583f38675e09ed6c22358260b927
kunalmkv <138765481+cd-nikhil@users.noreply.github.com>
2023-12-09 14:38:21 +0530

    frontend|stable|fixing-profile-page-navbar

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -8,6 +8,8 @@ import {
   Typography,
 } from "@mui/material";
 
+import Navbar from "../components/Navbar";
+
 const ProfilePage = () => {
   const [currentTab, setCurrentTab] = React.useState(0);
 
@@ -16,65 +18,77 @@ const ProfilePage = () => {
   };
 
   return (
-    <div
-      style={{
-        backgroundColor: "black",
-        color: "white",
-        minHeight: "100vh",
-        paddingTop:"30px",
-        width: "100vw",
-      }}
-    >
-      {/* Top Section - User Profile */}
-      <div style={{ display: "flex", alignItems: "center", marginTop: "20px", marginLeft:"50px" }}>
-        <Avatar
-          alt="User Avatar"
-          src="https://via.placeholder.com/150"
-          sx={{ width: 150, height: 150, marginRight: "20px" }}
-        />
-        <div>
-          <Typography variant="h4">0x1324...3242</Typography>
-          <Typography variant="subtitle1">Secured</Typography>
-        </div>
-      </div>
-
-      {/* Bottom Section - Tabs (Portfolio and Feed) */}
-      <div style={{ marginTop: "40px", marginLeft:"50px" }}>
-        <AppBar
-          position="static"
-          color="default"
-          sx={{ backgroundColor: "black" }}
+    <>
+      <Navbar />
+      <div
+        style={{
+          backgroundColor: "black",
+          color: "white",
+          minHeight: "100vh",
+          paddingTop: "10px",
+          width: "100vw",
+        }}
+      >
+        {/* Top Section - User Profile */}
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            marginTop: "20px",
+            marginLeft: "30px",
+          }}
         >
-          <Tabs
-            value={currentTab}
-            onChange={handleTabChange}
-            indicatorColor="primary"
-            textColor="primary"
+          <Avatar
+            alt="User Avatar"
+            src="https://via.placeholder.com/150"
+            sx={{ width: 120, height: 120, marginRight: "20px" }}
+          />
+          <div>
+            <Typography variant="h5">0x1324...3242</Typography>
+            <Typography variant="subtitle1">Secured</Typography>
+          </div>
+        </div>
+
+        {/* Bottom Section - Tabs (Portfolio and Feed) */}
+        <div style={{ marginTop: "30px", marginLeft: "30px" }}>
+          <AppBar
+            position="static"
+            color="default"
+            sx={{ backgroundColor: "black" }}
           >
-            <Tab
-              label="Portfolio"
-              sx={{ backgroundColor: "black", color: "white" }}
-            />
-            <Tab
-              label="Feed"
-              sx={{ backgroundColor: "black", color: "white" }}
-            />
-          </Tabs>
-        </AppBar>
-        <div style={{marginTop:"25px"}}>
-          {currentTab === 0 && (
-            <div>
-              <Typography variant="h6">Portfolio Content Goes Here</Typography>
-            </div>
-          )}
-          {currentTab === 1 && (
-            <div>
-              <Typography variant="h6">Feed Content Goes Here</Typography>
-            </div>
-          )}
+            <Tabs
+              value={currentTab}
+              onChange={handleTabChange}
+              indicatorColor="primary"
+              textColor="primary"
+            >
+              <Tab
+                label="Portfolio"
+                sx={{ backgroundColor: "black", color: "white" }}
+              />
+              <Tab
+                label="Feed"
+                sx={{ backgroundColor: "black", color: "white" }}
+              />
+            </Tabs>
+          </AppBar>
+          <div style={{ marginTop: "25px" }}>
+            {currentTab === 0 && (
+              <div>
+                <Typography variant="h6">
+                  Portfolio Content Goes Here
+                </Typography>
+              </div>
+            )}
+            {currentTab === 1 && (
+              <div>
+                <Typography variant="h6">Feed Content Goes Here</Typography>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
